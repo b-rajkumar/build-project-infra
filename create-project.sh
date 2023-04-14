005dd5abb45540579f9e562919a3d6fe37e71bcb
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-04-14 17:11:37 +0530

    Deleted project-template

--- create-project.sh
+++ create-project.sh
@@ -1,13 +1,13 @@
 PROJECT_NAME=$1
 
-cp -R ~/projects/build-project-infra/project-template ./$PROJECT_NAME
+mv ./project-template ./$PROJECT_NAME
 
 cd $PROJECT_NAME
 
 mv ./src/source.js ./src/$PROJECT_NAME.js
 mv ./test/test.js ./test/$PROJECT_NAME-test.js
 
-sed -i '' -e "s/test.js/$PROJECT_NAME-test.js/g" ./runtest.sh 
+sed -i '' -e "s/test.js/$PROJECT_NAME-test.js/g" ./run-test.sh 
 sed -i '' -e "s/source.js/$PROJECT_NAME.js/g" ./test/$PROJECT_NAME-test.js 
 
 git init
--- project-template/test/test.js
+++
@@ -1,5 +0,0 @@
-const test = require('../lib/test-framework.js');
-const sourceFunctions = require('../src/source.js');
-
-test.assertEquality(1, sourceFunctions.one, '1 should be one');
-test.assertEquality(2, sourceFunctions.one, '2 should not be one');
