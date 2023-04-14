309c4e94f09799ba09839136c17c67f34d55e346
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-04-14 17:22:57 +0530

    Updated to get project structure from git to create

--- create-project.sh
+++ create-project.sh
@@ -1,14 +1,40 @@
 PROJECT_NAME=$1
 
-mv ./project-template ./$PROJECT_NAME
+function downloading_directory_structure() {
+  git clone https://github.com/b-rajkumar/project-template.git
+  mv ./project-template ./$PROJECT_NAME
+  cd $PROJECT_NAME
+  rm -rf .git
+}
 
-cd $PROJECT_NAME
+function modifiying_file_names() {
+  mv ./src/source.js ./src/$PROJECT_NAME.js
+  mv ./test/test.js ./test/$PROJECT_NAME-test.js
 
-mv ./src/source.js ./src/$PROJECT_NAME.js
-mv ./test/test.js ./test/$PROJECT_NAME-test.js
+  sed -i '' -e "s/test.js/$PROJECT_NAME-test.js/g" ./run-test.sh 
+  sed -i '' -e "s/source.js/$PROJECT_NAME.js/g" ./test/$PROJECT_NAME-test.js 
+}
 
-sed -i '' -e "s/test.js/$PROJECT_NAME-test.js/g" ./run-test.sh 
-sed -i '' -e "s/source.js/$PROJECT_NAME.js/g" ./test/$PROJECT_NAME-test.js 
+function setup_git() {
+  git init 
+  git add src/ test/ lib/ runtest.sh .gitignore
+}
 
-git init
+function display_structure() {
+  echo "\nCreated directory structure :\n"
+  cd ..
+  tree $PROJECT_NAME
+  echo "\nExecuting runtest.sh to verify directory structure :\n"
+  cd $PROJECT_NAME
+  ./run-test.sh
+}
+
+function main() {
+  downloading_directory_structure
+  modifiying_file_names
+  setup_git
+  display_structure
+}
+
+main
 
