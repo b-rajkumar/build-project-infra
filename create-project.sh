8cbdb8d058191e3cdffcb1725e18412a3d82c130
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-17 12:58:18 +0530

    Updated to handle TODO.txt

--- create-project.sh
+++ create-project.sh
@@ -11,8 +11,7 @@ function modifiying_file_names() {
   mv ./src/source.js ./src/$PROJECT_NAME.js
   mv ./test/test.js ./test/$PROJECT_NAME-test.js
 
-  sed -i '' -e "s/test.js/$PROJECT_NAME-test.js/g" ./run-test.sh 
-  sed -i '' -e "s/source.js/$PROJECT_NAME.js/g" ./test/$PROJECT_NAME-test.js 
+  sed -i '' "s/test.js/$PROJECT_NAME-test.js/g" run-test.sh
 }
 
 function setup_git() {
